b44c1eb24be93a25f2ca195470059f649ab4d05a
HaidarKhalid <HaidarKhalid01@gmail.com>
2023-02-02 23:56:08 +0300

    cors added

--- src/server.js
+++ src/server.js
@@ -2,7 +2,10 @@ const express = require("express")
 const productRouter = require("./routes/productRoutes.js")
 const db = require("./db")
 const app = express()
+const cors = require("cors")
+
 app.use(express.json())
+app.use(cors())
 
 app.use("/products", productRouter)
 
