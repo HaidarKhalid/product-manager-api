6d773f8ff6eb903ba4b05a291441d0b08bf85359
HaidarKhalid <HaidarKhalid01@gmail.com>
2023-02-02 22:13:06 +0300

    fixed

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,8 +1,8 @@
 const ProductModel = require("../models/productModel.js")
 
 const createProduct = async (req, res) => {
-    const {name, price} = req.body;
-    const newProduct = await ProductModel.create({name, price})
+    const {name, price, youtubeReview, productWebsite, description} = req.body;
+    const newProduct = await ProductModel.create({name, price, youtubeReview, productWebsite, description})
     res.json(newProduct)
 }
 
@@ -14,8 +14,8 @@ const getAllProducts = async (req, res) => {
 
 const updateProduct = async (req, res) => {
     const {productId} = req.params
-    const {name, price} = req.body
-    const updatedProduct = await ProductModel.findByIdAndUpdate(productId, {name, price}, {new: true})
+    const {name, price, youtubeReview, productWebsite, description} = req.body
+    const updatedProduct = await ProductModel.findByIdAndUpdate(productId, {name, price, youtubeReview, productWebsite, description}, {new: true})
     res.json(updatedProduct)
 }
 
@@ -41,4 +41,4 @@ module.exports = {
     deleteProduct
 }
 
-//, youtubeReview, productWebsite, otherPhotos
+//, youtubeReview, productWebsite, description
--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose")
 const productShcema = mongoose.Schema({
     name: String,
-    price: Number
+    price: Number, 
+    youtubeReview: String,
+    productWebsite: String, 
+    description: String
 });
 
 const model = mongoose.model("Product", productShcema)
